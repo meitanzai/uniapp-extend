b4cb260d403802d9310c3b3f04832f45d27ab26a
梦之蓝 <630039906@qq.com>
2020-01-12 12:19:49 +0800

    H5 PC预览

--- App.vue
+++ App.vue
@@ -7,8 +7,8 @@
 			
 			uni.getSystemInfo({
 				success(e){
-					/* 窗口宽度 大于375px且不在PC页面时，跳转至PC页面 */
-					if(e.windowWidth>375 && !window.top.isPC){
+					/* 窗口宽度 大于420px且不在PC页面时，跳转至PC页面 */
+					if(e.windowWidth>420 && !window.top.isPC){
 						window.location.pathname = '/uniapp-extend/static/html/pc.html';
 					}
 				}
