dc092b6955c313227d3cee8898ae05e131ebbb04
梦之蓝 <630039906@qq.com>
2020-01-13 22:26:33 +0800

    无效文件

--- unpackage/dist/build/app-plus/pages/globalJs/globalJs.js
+++
@@ -1,2 +0,0 @@
-setCssToHead(["@charset \x22UTF-8\x22;\n.",[1],"container.",[1],"data-v-2a9ba5c8 { padding: ",[0,50],"; }\n.",[1],"container \x3e wx-view.",[1],"data-v-2a9ba5c8 { margin-bottom: ",[0,30],"; }\n",],undefined,{path:"./pages/globalJs/globalJs.wxss"})();
-document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/globalJs/globalJs.wxml') } }));
