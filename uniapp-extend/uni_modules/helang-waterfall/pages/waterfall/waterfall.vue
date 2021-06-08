014908c62dbc460fc7377b7f840c2181c907e71d
梦之蓝 <630039906@qq.com>
2021-06-09 00:14:36 +0800

    feat(瀑布流):101

--- uniapp-extend/uni_modules/helang-waterfall/pages/waterfall/waterfall.vue
+++ uniapp-extend/uni_modules/helang-waterfall/pages/waterfall/waterfall.vue
@@ -151,45 +151,60 @@
 					return;
 				}
 
+				// 左右列表高度的差
+				let differ = this.leftHeight - this.rightHeight;
+				// 计算差值，用于确定优先插入那一边
+				let differVal = 0;
+								
 				// 初始化左右列表的数据
-				let i = this.leftHeight > this.rightHeight ? 1 : 0;
+				let i = differ > 0 ? 1 : 0;
+				
 				let [left, right] = [
 					[],
 					[]
 				];
 				
-				// 左右列表高度的差
-				let differ = this.leftHeight - this.rightHeight;
 				
-				// 将数据源分为左右两个列表，容错差值请自行根据项目中的数据情况调节
-				res.forEach((item, index) => {
+				// 获取插入的方向
+				let getDirection = (index)=>{
 					/* 左侧高度大于右侧超过 600px 时，则前3条数据都插入到右边 */
-					if(differ>= 600 && index < 3){		
-						right.push(item);
-						return;
+					if(differ>= 600 && index < 3){
+						differVal = 1;
+						return 'right';
 					}
 					
 					/* 右侧高度大于左侧超过 600px 时，则前3条数据都插入到左边 */
-					if(differ <= -600 && index < 3){		
-						left.push(item);
-						return;
+					if(differ <= -600 && index < 3){
+						differVal = -1;
+						return 'left';
 					}
 					
 					/* 左侧高度大于右侧超过 350px 时，则前2条数据都插入到右边 */
-					if(differ >= 350 && index < 2){		
-						right.push(item);
-						return;
+					if(differ >= 350 && index < 2){
+						return 'right';
 					}
 					/* 右侧高度大于左侧超过 350px 时，则前2条数据都插入到左边 */
-					if(differ <= -350 && index < 2){		
-						left.push(item);
-						return;
+					if(differ <= -350 && index < 2){
+						differVal = -1;
+						return 'left';
 					}
 					
 					/* 当前数据序号为偶数时，则插入到左边 */
-					if (i % 2 == 0) {
+					if ((i+differVal) % 2 == 0) {
+						return 'left';
+					} else {
+						/* 当前数据序号为奇数时，则插入到右边 */
+						return 'right';
+					}
+				}
+				
+				// 将数据源分为左右两个列表，容错差值请自行根据项目中的数据情况调节
+				res.forEach((item, index) => {
+					if(getDirection(index) == 'left'){
+						//console.log(`差值：${differ},方向：left，序号${index}`)
 						left.push(item);
-					} else {		/* 当前数据序号为奇数时，则插入到右边 */
+					}else{
+						//console.log(`差值：${differ},方向：right，序号${index}`)
 						right.push(item);
 					}
 					i++;
